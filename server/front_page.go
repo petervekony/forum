466b12219b088604132eff3da2faadd00fdcfe3a
adrian1206 <adrian.lui.fi@gmail.com>
2022-11-11 16:14:41 +0200

    Adrian made front end changes

--- server/front_page.go
+++ server/front_page.go
@@ -4,28 +4,49 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+	"strings"
+	"os"
+	"log"
+	"gritface/database"
 )
 
 func FrontPage(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+	if r.URL.Path == "/" { // TBC for session check
+		fmt.Println("cookies handling.")
+		uid, err := sessionManager.checkSession(w, r)
+		if err != nil {
+			// Handle error for session check fail
+		}
+	
+		tmpl, err := template.ParseFiles("server/public_html/index.html")
+		if err != nil {
+			//error404(w)
+			return
+		}
+	
+		dyn_content := make(map[string]string)
+		dyn_content["name"] = uid
+	
+		tmpl.Execute(w, dyn_content)
+	} else if strings.Contains(r.URL.Path, "/server/") {
+		script, err := os.ReadFile(r.URL.Path[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write(script)
+	} else if r.URL.Path == "/posts" {
+		db, err := database.DatabaseExist()
+		if err != nil {
+			log.Fatal(err)
+		}
+		posts, err := database.Retrieve20Posts(db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write([]byte(posts))
+	} else {
 		fmt.Println("Trying to reach unknown path ", r.URL.Path)
 		//error404(w)
 		return
-	}
-
-	uid, err := sessionManager.checkSession(w, r)
-	if err != nil {
-		// Handle error for session check fail
-	}
-
-	tmpl, err := template.ParseFiles("server/public_html/index.html")
-	if err != nil {
-		//error404(w)
-		return
-	}
-
-	dyn_content := make(map[string]string)
-	dyn_content["name"] = uid
-
-	tmpl.Execute(w, dyn_content)
+	}	
 }
--- server/public_html/static/js/script.js
+++ server/public_html/static/js/script.js
@@ -13,46 +13,54 @@ async function initPage() {
           const postImage = document.createElement("div");
           const postComments = document.createElement("div");
           const postReactions = document.createElement("div");
-  
-          //test
-          const a = document.createElement("div");
-          const b = document.createElement("div");
-          const c = document.createElement("div");
-          const d = document.createElement("div");
+          const postRow = document.createElement("div");
+          const postLike = document.createElement("button");
+          const postHeart = document.createElement("button");
+          const postComment = document.createElement("div");
+          const postCommentImg = document.createElement("div");
   
           postDiv.classList.add(
             "col-8",
             "offset-2",
-            "text-center",
+            "text-white",
             "border",
             "rounded",
             "my-2",
             "py-2"
           );
-          postBody.classList.add("postBody")
-          postHeading.classList.add("postHeading")
-          postInsertTime.classList.add("postInsertTime")
-          postModTime.classList.add("postModTime")
-          postComments.classList.add("postComments")
-          postReactions.classList.add("postReactions")
-          postImage.classList.add("postImage")
+
+          postBody.classList.add("col-10", "offset-1");
+          postHeading.classList.add("col-10", "offset-1", "my-2");
+          postInsertTime.classList.add("col-6", "order-0", "text-left");
+          postModTime.classList.add("col-6", "order-1", "text-end");
+          postComments.classList.add("row", "my-2");
+          postReactions.classList.add("col-5", "mx-1");
+          postImage.classList.add("border", "bg-info", "text-center");
+          postLike.classList.add("btn", "border", "rounded", "bg-dark");
+          postHeart.classList.add("btn", "border", "rounded", "bg-dark");
+          postComment.classList.add("col-9", "border", "rounded", "bg-secondary");
+          postCommentImg.classList.add("col-2", "mx-1", "border", "rounded", "bg-info");
   
-          postBody.textContent = postJSON.post_body;
-          postHeading.textContent = postJSON.post_heading;
-          postInsertTime.textContent = postJSON.post_insert_time;
-          postModTime.textContent = postJSON.post_mod_time;
+          postBody.textContent = postJSON.body;
+          postHeading.textContent = postJSON.heading;
+          postInsertTime.textContent = postJSON.insert_time;
+          postModTime.textContent = postJSON.update_time;
           postComments.textContent = postJSON.post_comments;
           postReactions.textContent = postJSON.post_reactions;
-          postImage.textContent = postJSON.post_image;
+          postImage.textContent = postJSON.image;
   
           postSection.appendChild(postDiv);
           postDiv.appendChild(postHeading);
           postDiv.appendChild(postBody);
-          postDiv.appendChild(postReactions);
-          postDiv.appendChild(postComments);
+          postDiv.appendChild(postRow)
           postBody.appendChild(postImage);
           postBody.appendChild(postInsertTime);
           postBody.appendChild(postModTime);
+          postRow.appendChild(postReactions);
+          postRow.appendChild(postComments);
+          postReactions.appendChild(postLike);
+          postReactions.appendChild(postHeart);
+          console.log(postJSON)
         });
       });
   }

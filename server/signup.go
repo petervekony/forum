2653cf9ecf88cd026cdb23a4ae0df26c522f852d
adrian1206 <adrian.lui.fi@gmail.com>
2022-11-14 14:28:14 +0200

    fixed email regex by adrian"

--- server/signup.go
+++ server/signup.go
@@ -10,7 +10,7 @@ import (
 
 // function to check if email is valid
 func isEmailValid(e string) bool {
-	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,4}$`)
 	return emailRegex.MatchString(e)
 }
 

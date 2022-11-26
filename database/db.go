f75895b567abddd326e93a0018e7b88ba3f57bfa
dicarste <dcco@hotmail.com>
2022-11-26 19:26:06 +0200

    Shit that should not be included in master basic

--- database/db.go
+++ database/db.go
@@ -5,9 +5,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -101,7 +103,7 @@ func DbConnect() (*sql.DB, error) {
 // function check if database exists, if not it creates it, if it does it opens it
 func DatabaseExist() (*sql.DB, error) {
 	newDb := false
-	databaseFile := "forum-db.db"
+	databaseFile := "forum-db.s3db"
 	_, err := os.Stat(databaseFile)
 	if os.IsNotExist(err) {
 		fmt.Println("Creating the forum database ...")
@@ -115,7 +117,15 @@ func DatabaseExist() (*sql.DB, error) {
 	} else if err != nil {
 		return nil, err
 	}
-	forumdb, err := sql.Open("sqlite3", "./"+databaseFile+"?_auth&_auth_user=forum&_auth_pass=forum&_auth_crypt=sha1")
+
+	sql.Register("sqlite3_log", &sqlite3.SQLiteDriver{
+		ConnectHook: func(conn *sqlite3.SQLiteConn) error {
+			log.Printf("Auth enabled: %v\n", conn.AuthEnabled())
+			return nil
+		},
+	})
+
+	forumdb, err := sql.Open("sqlite3_log", "file:"+databaseFile+"?_auth&_auth_user=forum&_auth_pass=forum&_auth_crypt=sha1")
 	// Open the created Sqlite3 File
 	if err != nil {
 		return nil, err
